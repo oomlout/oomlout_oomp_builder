50e42496d1b617b451b068574f599dde955136ff
Oom Lout <aaron@oomlout.com>
2023-12-07 12:22:13 +0000

    Update action_all_default.py

--- action_all_default.py
+++ action_all_default.py
@@ -6,15 +6,16 @@ utilities = []
 utilities.append("oomlout_oomp_utility_readme_generation")
 
 def main(**kwargs):
-    action_create.main(**kwargs)
-    action_load.main(**kwargs)    
+    #action_create.main(**kwargs)
+    #action_load.main(**kwargs)    
     #action_make_links.main(**kwargs)
 
-    for utility in utilities:
-        utility_module = __import__(f"temporary/{utility}")
-        utility_module.main(**kwargs)
+    #for utility in utilities:
+    #    utility_module = __import__(f"temporary/{utility}/working")
+    #    utility_module.main(**kwargs)
 
-
+    import temporary.oomlout_oomp_utility_readme_generation.working
+    temporary.oomlout_oomp_utility_readme_generation.working.main(**kwargs)
 
 
 
